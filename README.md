9b8406ca1423ba9faf4ef6ce9db16c9abcfc0a5c
Gabriel Huber <mail@gabrielhuber.at>
2023-03-03 02:13:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Package scanner 2
+
+Package scanner for the nimble package list.
+
+## Features
+
+Can scan changed packages only and runs in offline mode by default.
+
+Checks packages for:
+
+* Duplicate and invalid names
+* Missing alias targets
+* Empty tags
+* Invalid method
+* Missing description or license
+* Unavailable URLs
+* Insecure URLs
+
+## Copyright
+
+Based on package_scanner.nim by Federico Ceratto
+
+Released under [GPLv3 License](LICENSE.txt)
---
+++ packagescanner2.nim
@@ -0,0 +1,206 @@
+import std/parseopt
+import std/os
+import std/json
+import std/tables
+import std/strutils
+import std/httpclient
+import std/streams
+import std/net
+
+
+const usage = """
+Usage: packagescanner2 <packages.json> [--old=packages_old.json] --fetch
+Scans the nimble package list for mistakes and dead packages.
+Options:
+  --old=   Old package file, will only scan changed packages
+  --fetch  Try to request the package url
+  --help   Print this help text"""
+
+
+proc checkUrlReachable(client: HttpClient, url: string): string =
+  var headers: HttpHeaders = nil
+  if url.startsWith("https://github.com"):
+    if existsEnv("GITHUB_TOKEN"):
+      headers = newHttpHeaders({"Authorization": "Bearer " & getEnv("GITHUB_TOKEN")})
+  
+  try:
+    let resp = client.request(url, headers=headers)
+    discard resp.bodyStream.readAll()
+    if not resp.code.is2xx:
+      result = "Server returned status " & $resp.code
+  except TimeoutError:
+    result = "Timeout after " & $client.timeout & " seconds"
+    client.close()
+  except HttpRequestError:
+    result = "HTTP error: " & getCurrentExceptionMsg()
+    client.close()
+  except AssertionDefect:
+    result = "httpclient error: " & getCurrentExceptionMsg()
+    client.close()
+  except CatchableError as e:
+    result = "Unexpected exception " & $e.name & ": " & e.msg
+    client.close()
+
+template logPackageError(errorMsg: string) =
+  echo "E: ", errorMsg
+  success = false
+
+template checkUrl(urlType: string, url: string) =
+  if url == "":
+    logPackageError(displayName & " has an empty " & urlType & " URL")
+  elif not url.startsWith("https://"):
+    logPackageError(displayName & " has a non-https " & urlType & " URL: " & url)
+  elif fetch and not isDeleted:
+    let urlError = client.checkUrlReachable(url)
+    if urlError != "":
+      logPackageError(displayName & " has an unreachable " & urlType & " URL: " & url)
+      logPackageError(urlError)
+
+proc getStrIfExists(n: JsonNode, name: string, default: string = ""): string =
+  if n.hasKey(name) and n[name].kind == JString:
+    result = n[name].str
+
+proc getElemsIfExists(n: JsonNode, name: string, default: seq[JsonNode] = @[]): seq[JsonNode] =
+  if n.hasKey(name) and n[name].kind == JArray:
+    result = n[name].elems
+
+proc checkPackages(newPackagesPath: string, oldPackagesPath: string, fetch: bool = false): int =
+  var oldPackagesTable = initTable[string, JsonNode]()
+  if oldPackagesPath != "":
+    let oldPackagesJson = parseJson(readFile(oldPackagesPath))
+    for oldPkg in oldPackagesJson:
+      let oldNameLower = oldPkg.getStrIfExists("name").toLowerAscii()
+      if oldNameLower != "":
+        oldPackagesTable[oldNameLower] = oldPkg
+  
+  let newPackagesJson = parseJson(readFile(newPackagesPath))
+  # Do a first pass through the list to count duplicate names
+  var packageNameCounter = initCountTable[string]()
+  for pkg in newPackagesJson:
+    let pkgNameLower = pkg.getStrIfExists("name").toLowerAscii()
+    if pkgNameLower != "":
+      packageNameCounter.inc(pkgNameLower)
+  
+  var client: HttpClient = nil
+  if fetch:
+    client = newHttpClient(timeout=3000)
+    client.headers = newHttpHeaders({"User-Agent": "Nim packge_scanner/2.0"})
+  
+  var failedPackages = 0
+  for pkg in newPackagesJson:
+    var success = true  # Set to false by logPackageError
+    let pkgName = pkg.getStrIfExists("name")
+    let pkgNameLower = pkgName.toLowerAscii()
+    var displayName = pkgName
+    if displayName == "":
+      displayName = "<unnamed package>"
+    
+    # Start with detecting duplicates
+    if packageNameCounter[pkgNameLower] > 1:
+      let url = pkg.getStrIfExists("url", "<no url>")
+      logPackageError("Duplicate package " & displayName & " from url " & url)
+    
+    # isNew should be used in future versions to do a conditional inspection
+    # of the package contents which requires downloading the full release tarball
+    let isNew = not oldPackagesTable.hasKey(pkgNameLower)
+    var isModified: bool
+    if isNew:
+      isModified = true
+    else:
+      isModified = oldPackagesTable[pkgNameLower] != pkg
+    
+    if isModified:
+      if pkgName == "":
+        logPackageError("Missing package name")
+        
+      let isAlias = pkg.hasKey("alias")
+      
+      if isAlias:
+        if packageNameCounter[pkg["alias"].getStr().toLowerAscii()] == 0:
+          logPackageError(displayName & " is an alias pointing to a missing package")
+      else:
+        if not pkgName.validIdentifier():
+          logPackageError(displayName & " is not a valid identifier")
+        
+        var tags = pkg.getElemsIfExists("tags")
+        var isDeleted = false
+        if tags.len == 0:
+          logPackageError(displayName & " has no tags")
+        else:
+          var emptyTags = false
+          for tag in tags:
+            if tag.getStr == "":
+              emptyTags = true
+            if tag.getStr.toLowerAscii() == "deleted":
+              isDeleted = true
+          if emptyTags:
+            logPackageError(displayName & " has empty tags")
+        
+        if not pkg.hasKey("method"):
+          logPackageError(displayName & " has no method")
+        elif pkg["method"].kind != JString or pkg["method"].str notin ["git", "hg"]:
+          logPackageError(displayName & " has an invalid method")
+          
+        if pkg.getStrIfExists("description") == "":
+          logPackageError(displayName & " has no description")
+        
+        if pkg.getStrIfExists("license") == "":
+          logPackageError(displayName & " has no license")
+        
+        var downloadUrl = pkg.getStrIfExists("url")
+        if not pkg.hasKey("url"):
+          logPackageError(displayName & " has no download URL")
+        else:
+          downloadUrl = downloadUrl
+          checkUrl("download", downloadUrl)
+        
+        if pkg.hasKey("web"):
+          let webUrl = pkg["web"].getStr()
+          if webUrl != downloadUrl:
+            checkUrl("web", webUrl)
+        
+        if pkg.hasKey("doc"):
+          let docUrl = pkg["doc"].getStr()
+          if docUrl != downloadUrl:
+            checkUrl("doc", docUrl)
+    
+    if not success:
+      inc failedPackages
+
+  if failedPackages > 0:
+    echo "\nProblematic packages count: ", failedPackages
+    result = 1
+  
+
+proc cliMain(): int = 
+  var parser = initOptParser(os.commandLineParams())
+  var newPackagesPath = ""
+  var oldPackagesPath = ""
+  var fetch = false
+  while true:
+    parser.next()
+    case parser.kind:
+    of cmdEnd: break
+    of cmdShortOption, cmdLongOption:
+      if parser.key == "old":
+        oldPackagesPath = parser.val
+      elif parser.key == "fetch":
+        fetch = true
+      elif parser.key == "help":
+        echo usage
+        return 0
+    of cmdArgument:
+      if newPackagesPath == "":
+        newPackagesPath = parser.key
+      else:
+        echo "Too many arguments!"
+        return 1
+
+  if newPackagesPath == "":
+    echo usage
+    return 1
+
+  result = checkPackages(newPackagesPath, oldPackagesPath, fetch)
+
+when isMainModule:
+  quit(cliMain())
